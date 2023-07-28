ea57620517ea07d2b36d164a3ff053a437282b05
Atharva Gupta <atharva.gu@gmail.com>
2023-07-28 19:47:34 +0400

    Defining road in javascript

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const canvas = document.getElementById("canvas1");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width/2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -11,6 +12,7 @@ animate();
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     //calls animate method over and over again
     requestAnimationFrame(animate);
