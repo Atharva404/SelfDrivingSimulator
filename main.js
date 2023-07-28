1eef4e359f1451cf49219de4d1e08a6adcad250b
Atharva Gupta <atharva.gu@gmail.com>
2023-07-28 23:19:34 +0400

    add sensor to car

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ canvas.width = 200;
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width/2, canvas.width * 0.9);
 const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-car.draw(ctx);
+// car.draw(ctx);
 
 animate();
 
@@ -14,7 +14,7 @@ function animate() {
     canvas.height = window.innerHeight;
 
     ctx.save();
-    ctx.translate(0, -car.y);
+    ctx.translate(0, -car.y + canvas.height * 0.7);
 
     road.draw(ctx);
     car.draw(ctx);
