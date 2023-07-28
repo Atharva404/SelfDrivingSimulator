9f21fc7a7476d0fd550065c40cb69baaf1d0b3db
Atharva Gupta <atharva.gu@gmail.com>
2023-07-28 17:34:10 +0400

    Creating car file

---
+++ car.js
@@ -0,0 +1,5 @@
+class Car {
+    constructor(x, y, width, height) {
+        
+    }
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+// Creating the road area on webpage
+const canvas = document.getElementById("canvas1");
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
