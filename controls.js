fa58abd20b8eec9ad17547ec59f8644309058c24
Atharva Gupta <atharva.gu@gmail.com>
2023-07-29 21:24:29 +0400

    Remove key events

--- controls.js
+++ controls.js
@@ -7,49 +7,9 @@ class Controls {
 
 
         switch(type) {
-            case "KEYS":
-                this.#addKeyboardListener();
-                break;
             case "DUMMY":
                 this.forward = true;
                 break;
-        }
-    }
-
-    #addKeyboardListener() {
-        document.onkeydown=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left = true;
-                    break;
-                case "ArrowRight":
-                    this.right = true;
-                    break;
-                case "ArrowUp":
-                    this.forward = true;
-                    break;
-                case "ArrowDown":
-                    this.reverse = true;
-                    break;
-            }
-            console.table(this);
-        }
-        document.onkeyup=(event)=>{
-            switch(event.key){
-                case "ArrowLeft":
-                    this.left = false;
-                    break;
-                case "ArrowRight":
-                    this.right = false;
-                    break;
-                case "ArrowUp":
-                    this.forward = false;
-                    break;
-                case "ArrowDown":
-                    this.reverse = false;
-                    break;
-            }
-            console.table(this);
         }
     }
 }
