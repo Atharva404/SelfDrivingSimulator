47d24ba5171b4e885776b9b247e96dc0d0bd3250
Atharva Gupta <atharva.gu@gmail.com>
2023-07-29 13:09:52 +0400

    Create sensors that detect borders

--- car.js
+++ car.js
@@ -10,6 +10,7 @@ class Car {
         this.maxSpeed=3;
         this.friction = 0.05;
         this.angle = 0;
+        this.damaged = false;
 
         this.sensor = new Sensor(this);
         this.controls = new Controls();
@@ -17,7 +18,41 @@ class Car {
 
     update(roadBorders) {
         this.#move();
+        this.polygon = this.#createPolygon();
+        this.damaged = this.#assessDamage(roadBorders);
         this.sensor.update(roadBorders);
+    }
+
+    #assessDamage(roadBorders) {
+        for (let i = 0; i < roadBorders.length; i++) {
+            if (polyInteresect(this.polygon, roadBorders[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height)/2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x:this.x-Math.sin(this.angle-alpha)*rad, 
+            y:this.y-Math.cos(this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(this.angle+alpha)*rad, 
+            y:this.y-Math.cos(this.angle+alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI + this.angle-alpha)*rad, 
+            y:this.y-Math.cos(Math.PI + this.angle-alpha)*rad
+        });
+        points.push({
+            x:this.x-Math.sin(Math.PI + this.angle+alpha)*rad, 
+            y:this.y-Math.cos(Math.PI + this.angle+alpha)*rad
+        });
+        return points;
     }
 
     #move() {
@@ -57,21 +92,52 @@ class Car {
         this.y -= Math.cos(this.angle) * this.speed;
     }
     draw(ctx) {
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-
+        if (this.damaged) {
+            ctx.fillStyle = "gray";
+        } else {
+            ctx.fillStyle = "black";
+        }
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2,
-            -this.height/2,
-            this.width,
-            this.height
-        );
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for (let i = 1; i < this.polygon.length; i++) {
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
         ctx.fill();
-
-        ctx.restore();
-
         this.sensor.draw(ctx);
     }
+}
+function getInteresection(A, B, C, D) {
+    const tTop = (D.x-C.x) * (A.y-C.y)-(D.y-C.y) * (A.x-C.x);
+    const uTop = (C.y-A.y) * (A.x-B.x)-(C.x-A.x) * (A.y-B.y);
+    const bottom = (D.y-C.y) * (B.x-A.x)-(D.x-C.x) * (B.y-A.y);
+
+    if (bottom !=0) {
+        const t =tTop/bottom;
+        const u=uTop/bottom;
+        if (t>=0 && t <=1 && u>=0 &&u<=1) {
+            return {
+                x:lerp(A.x, B.x, t),
+                y: lerp(A.y, B.y, t),
+                offset: t
+            }
+        }
+    }
+    return null;
+}
+
+function polyInteresect(poly1, poly2) {
+    for (let i = 0; i < poly1.length; i++) {
+        for (let j = 0; j < poly2.length; j++) {
+            const touch = getInteresection(
+                poly1[i],
+                poly1[(i+1)%poly1.length],
+                poly2[j],
+                poly2[(j+1)%poly2.length]
+            );
+            if (touch) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
